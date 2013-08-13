2c5604d83fc9a61246f3bf79c8c42ce35b98fee6
Adam Schwalm <adamschwalm@gmail.com>
2013-08-13 10:42:25 -0500

    Added generator

--- genome.hpp
+++ genome.hpp
@@ -4,22 +4,52 @@
 #include <cassert>
 #include <functional>
 #include <algorithm>
+#include <iostream>
 
 namespace evolve
-{    
-    namespace Selectors
+{
+
+    template<typename genome>
+    class Generator
     {
+    public:
+        typedef typename genome::value_type allele;
+        
+        Generator(std::function<allele(void)> _gen,
+                  unsigned int _num) :
+            gen(_gen),
+            num(_num){}
+
+        genome operator()() {
+            genome g;
+
+            for(auto i=0; i < num; i++) {
+                g.insert(g.end(), gen());
+            }
+
+            return g;
+       }
+
+    protected:
+        std::function<allele(void)> gen;
+        unsigned int num;
+
+    };
+    
+    namespace Selectors
+    {      
         template<typename genome, size_t num>
         inline void top(std::vector<genome>& population,
                         std::function<float(const genome&)> evaluator)
         {
+            static_assert( num >=2, "Selector must leave at least 2 individuals in the population");
             assert(population.size() >= num );
 
             std::sort(population.begin(), population.end(),
                       [&](const genome& left,
                           const genome& right)
                       {
-                          return evaluator(left) < evaluator(right);
+                          return evaluator(left) > evaluator(right);
                       });
             
             auto start = population.begin();
@@ -52,9 +82,23 @@ namespace evolve
         namespace Crossover
         {
             template<typename genome>
-            inline void singlePoint(genome& g1,
-                                    genome& g2)
+            inline genome singlePoint(const genome& g1,
+                                      const genome& g2)
             {
+                genome g;
+
+                auto size = std::min(g1.size(), g2.size());
+                auto location = rand() % size;
+
+                auto g1location = g1.begin();
+                auto g2location = g2.begin();
+                
+                std::advance(g1location, location);
+                std::advance(g2location, location);
+                
+                g.insert(g.begin(), g1.begin(), g1location);
+                g.insert(g.end(), g2location, g2.end());
+                return g;
                 
             }
         }
@@ -77,34 +121,68 @@ namespace evolve
     class SimpleGA
     {
     public:
-        SimpleGA(std::function<float(const genomeType&)> _evaluator,
-                 std::function<void(genomeType&,
-                                    genomeType&)> _crossover,
+        SimpleGA(Generator<genomeType> _generator,
+                 std::function<float(const genomeType&)> _evaluator,
+                 std::function<genomeType(const genomeType&,
+                                          const genomeType&)> _crossover,
                  std::function<void(genomeType&)> _mutator,
                  std::function<void(std::vector<genomeType>&,
                                     std::function<float(const genomeType&)>)> _selector,
                  unsigned long _generations = 10000UL) :
+            generator(_generator),
             evaluator(_evaluator),
             crossover(_crossover),
+            mutator(_mutator),
             selector(_selector),
             generations(_generations){}
+
+        genomeType& getRandomIndividual() {
+            return population[rand() % population.size()];
+        }
             
+        
         void run()
         {
+            for(auto generation=0; generation < generations; ++generation)
+            {
+                for(auto i=0; i < popSize; ++i)
+                {
+                    population.push_back(generator());
+                }
+
+                selector(population, evaluator);
+
+                while(population.size() < popSize) {
+                    population.push_back(crossover(getRandomIndividual(),
+                                                   getRandomIndividual()));
+                }
+
+                for(auto member : population) {
+                    mutator(member);
+                }
+
+                std::cout << "Fitness:" << evaluator(population[0]) << std::endl;
+                for(auto allele : population[0])
+                {
+                    std::cout << allele << " ";
+                }
+                std::cout << std::endl;
+            }
         }
 
+                        
         void setPopulation(const std::array<genomeType, popSize>& _population) {
             population = _population;
         }
 
         std::array<genomeType, popSize>& getPopulation() const {return population;}
-
         
     protected:
-        std::array<genomeType, popSize> population;
+        std::vector<genomeType> population;
+        Generator<genomeType> generator;
         std::function<float(const genomeType&)> evaluator;
-        std::function<void(genomeType&,
-                           genomeType&)> crossover;
+        std::function<genomeType(const genomeType&,
+                                 const genomeType&)> crossover;
         std::function<void(genomeType&)> mutator;
         std::function<void(std::vector<genomeType>&,
                            std::function<float(const genomeType&)>)> selector;
--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
 
-
 #include "cppEvolve.hpp"
+#include <ctime>
 
 using namespace evolve;
 
+int foo()
+{
+    return rand() % 20;
+};
+
+
 int main()
 {
+    srand(time(NULL));
     typedef ListGenome::List1D<int> genome;
 
-    SimpleGA<genome, 400> ga(ListGenome::Evaluator::sum<genome>,
+    Generator<genome> generator(foo, 30);
+    
+    SimpleGA<genome, 400> ga(generator,
+                             ListGenome::Evaluator::sum<genome>,
                              ListGenome::Crossover::singlePoint<genome>,
                              ListGenome::Mutator::swap<genome>,
                              Selectors::top<genome, 30>,
-                             10000UL);
+                             10);
 
     ga.run();
     
