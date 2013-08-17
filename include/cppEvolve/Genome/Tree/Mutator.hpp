500f30c4b18cc3c9adcc18794189f8352e00bb39
Adam Schwalm <adamschwalm@gmail.com>
2013-08-16 22:38:16 -0500

    Completed randomNode mutator

--- include/cppEvolve/Genome/Tree/Mutator.hpp
+++ include/cppEvolve/Genome/Tree/Mutator.hpp
@@ -12,9 +12,34 @@ namespace evolve
             template<typename T>
             void randomNode(Tree<T>* tree, const TreeFactory<T>& factory)
             {
+                auto treeDepth = tree->getDepth();
+                auto depth = rand() % treeDepth;
 
-                delete tree->root;
-                tree->root = factory.createRandomSubTree();
+                auto currentNode = tree->root;
+                BaseNode<T>* prevNode = nullptr;
+
+                for (unsigned int i=0; i < depth; ++i)
+                {
+                    if (currentNode->getChildren().empty()) {
+                        auto& children = prevNode->getChildren();
+                        auto location = std::find(children.begin(), children.end(), currentNode);
+
+                        delete currentNode;
+                        *location = factory.createRandomNode();
+                        return;
+                    }
+                    else {
+                        prevNode = currentNode;
+                        currentNode = currentNode->getChildren()[rand() % currentNode->getChildren().size()];
+                    }
+                }
+
+                if (!currentNode->getChildren().empty()) {
+                    auto childValue = rand() % currentNode->getChildren().size();
+                    delete currentNode->getChildren()[childValue];
+
+                    currentNode->getChildren()[childValue] = factory.createRandomSubTree(treeDepth - depth);
+                }
 
             }
         }
